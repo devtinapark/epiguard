f279a835f0e8b7702a29268521f9be315654fbd2
Christine Tina Park <escp1004jk@gmail.com>
2024-06-14 15:34:20 +0700

    first files commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,6 +24,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="me"
+        options={{
+          title: 'Me',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
